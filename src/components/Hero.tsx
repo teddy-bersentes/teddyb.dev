76a5fd90582ae3f8dee3c5b9835374dedbb1c910
Josh Leichty <theodore.bersentes@gmail.com>
2023-05-21 02:35:05 -0400

    Last hero adjustments

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,21 +28,40 @@ function Hero() {
 
 	return (
 		<div className='relative flex flex-col items-center justify-center w-full pt-28'>
-			<div className='max-w-[52.3125rem] flex flex-col justify-center items-center relative'>
-				<p className='text-xl font-semibold text-center'>Hi, I&apos;m Teddy</p>
-				<h1 className='text-5xl font-bold max-w-[52.3125rem] text-center mt-4 leading-[4.5rem]'>
-					I specialize in
-					{' '}
-					<Color variant='red'>ideating</Color>,
-					{' '}
-					<Color variant='blue'>composing</Color>, and
-					{' '}
-					<Color variant='yellow'>realizing</Color> innovative apps
-				</h1>
+			<motion.div
+				className='max-w-[52.3125rem] flex flex-col justify-center items-center relative'
+				initial={{ opacity: 0, y: -20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{
+					type: 'spring',
+					damping: 10,
+					stiffness: 100,
+					velocity: 2,
+				}}
 
-				<Button className='mt-10'>
-					Show me more
-				</Button>
+			>
+				<motion.div
+					className='flex flex-col items-center justify-center w-full'
+					key='text'
+					initial={{ opacity: 0, y: -20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ delay: 0.2 }}
+				>
+					<p className='text-xl font-semibold text-center'>Hi, I&apos;m Teddy</p>
+					<h1 className='text-5xl font-bold max-w-[52.3125rem] text-center mt-4 leading-[4.5rem]'>
+						I specialize in
+						{' '}
+						<Color variant='red'>ideating</Color>,
+						{' '}
+						<Color variant='blue'>composing</Color>, and
+						{' '}
+						<Color variant='yellow'>realizing</Color> innovative apps
+					</h1>
+
+					<Button className='mt-10'>
+						Show me more
+					</Button>
+				</motion.div>
 
 				<Scribble.Twist />
 				<Scribble.Shapes />
@@ -53,7 +72,7 @@ function Hero() {
 				<Scribble.Butterfly />
 				<Scribble.ArrowCurve />
 				<Scribble.ArrowZigZag />
-			</div>
+			</motion.div>
 		</div>
 	);
 }
--- src/components/scribbles/Sparkle.tsx
+++ src/components/scribbles/Sparkle.tsx
@@ -8,7 +8,7 @@ export default function Sparkle() {
 			<motion.path
 				initial={{ scale: 1, rotate: 0.02 }}
 				animate={{ scale: [0.8, 1.2], rotate: 0.02 }}
-				transition={{ duration: 1.3, ...infiniteTransition }}
+				transition={{ duration: 1.1, ...infiniteTransition }}
 				d="M30.47 16.6996C37.3677 13.2861 42.6396 7.28832 45.14 0.0096237C45.3 -0.430376 47.9 14.3496 56.66 17.1996C56.66 17.1996 46.06 22.2796 43.55 32.3396C43.56 32.4796 42.95 24.3496 30.47 16.6996Z"
 				fill="black"
 			/>
@@ -16,7 +16,7 @@ export default function Sparkle() {
 			<motion.path
 				initial={{ scale: 1, rotate: 0.02 }}
 				animate={{ scale: [0.8, 1.2], rotate: 0.02 }}
-				transition={{ duration: 1.3, ...infiniteTransition, delay: 0.3 }}
+				transition={{ duration: 1.1, ...infiniteTransition, delay: 0.3 }}
 				d="M45.76 64.5696C51.4393 61.7554 55.7747 56.8088 57.82 50.8096C57.95 50.4496 60.08 62.6096 67.32 64.9496C67.32 64.9496 58.61 69.1296 56.54 77.3996C56.51 77.5096 56.01 70.8296 45.76 64.5696Z"
 				fill="black"
 			/>
@@ -24,7 +24,7 @@ export default function Sparkle() {
 			<motion.path
 				initial={{ scale: 1, rotate: 0.02 }}
 				animate={{ scale: [0.8, 1.3] }}
-				transition={{ duration: 1.3, ...infiniteTransition, delay: 0.6 }}
+				transition={{ duration: 1.1, ...infiniteTransition, delay: 0.6 }}
 				d="M0 46.0896C5.48176 43.3685 9.67087 38.5973 11.66 32.8096C11.78 32.4496 13.85 44.1896 20.84 46.4496C20.84 46.4496 12.42 50.4496 10.43 58.4496C10.38 58.5796 9.9 52.1296 0 46.0896Z"
 				fill="black"
 			/>
