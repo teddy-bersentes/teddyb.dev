183dbada72db425e5ad688dc3d6fc69958d1836f
Josh Leichty <theodore.bersentes@gmail.com>
2023-06-03 17:30:25 -0400

    OG image additions

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html lang="en" className='font-sans subpixel-antialiased scroll-smooth'>
-			<Head />
+			<Head>
+				<meta property="twitter:image" content="https://ucarecdn.com/947a7728-9875-4975-b8df-f90e829c8c6e/ogteddy.png" />
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:title" content="Teddy's Portfolio | Software Developer" />
+				<meta property="twitter:description" content="The portfolio of Teddy Bersentes, Software Developer." />
+
+
+				<meta property="og:image" content="https://ucarecdn.com/947a7728-9875-4975-b8df-f90e829c8c6e/ogteddy.png" />
+				<meta property="og:title" content="Teddy's Portfolio | Software Developer" />
+				<meta property="og:description" content="The portfolio of Teddy Bersentes, Software Developer." />
+				<meta property="og:url" content="https://www.teddyb.dev" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
