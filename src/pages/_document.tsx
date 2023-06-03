d193daa59d3c2ddc61918eb5d8a56f10d02c75d4
Josh Leichty <theodore.bersentes@gmail.com>
2023-06-03 17:32:07 -0400

    Changes to the OG image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
 	return (
 		<Html lang="en" className='font-sans subpixel-antialiased scroll-smooth'>
 			<Head>
-				<meta property="twitter:image" content="https://ucarecdn.com/947a7728-9875-4975-b8df-f90e829c8c6e/ogteddy.png" />
+				<meta property="twitter:image" content="https://ucarecdn.com/76f16958-c954-4b70-a97e-2e49789c4d5e/ognew.png" />
 				<meta property="twitter:card" content="summary_large_image" />
 				<meta property="twitter:title" content="Teddy's Portfolio | Software Developer" />
 				<meta property="twitter:description" content="The portfolio of Teddy Bersentes, Software Developer." />
 
 
-				<meta property="og:image" content="https://ucarecdn.com/947a7728-9875-4975-b8df-f90e829c8c6e/ogteddy.png" />
+				<meta property="og:image" content="https://ucarecdn.com/76f16958-c954-4b70-a97e-2e49789c4d5e/ognew.png" />
 				<meta property="og:title" content="Teddy's Portfolio | Software Developer" />
 				<meta property="og:description" content="The portfolio of Teddy Bersentes, Software Developer." />
 				<meta property="og:url" content="https://www.teddyb.dev" />
