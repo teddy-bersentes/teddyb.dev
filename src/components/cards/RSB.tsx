7395638caf1567c961b3258c076d93fceed73433
Josh Leichty <theodore.bersentes@gmail.com>
2023-06-03 04:20:00 -0400

    Ready. Steady. Article. (partially complete)

--- src/components/cards/RSB.tsx
+++ src/components/cards/RSB.tsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import Browser from "~/components/Browser"
 import Image from "next/image"
 import clsx from "clsx"
+import Link from "next/link"
 
 const browser: Variants = {
 	initial: { y: 0, rotate: 0.00 },
@@ -16,53 +17,55 @@ export default function RSB() {
 
 	return (
 		<div className='relative w-full lg:grow lg:w-auto'>
-			<motion.div
-				variants={card}
-				transition={transition}
-				initial='initial'
-				whileHover='hover'
-				className='relative flex flex-col w-full overflow-hidden bg-white pt-7 px-9 lg:grow lg:w-auto h-96 rounded-4xl hover:cursor-pointer'
-				onHoverStart={() => setHovering(true)}
-				onHoverEnd={() => setHovering(false)}
-			>
-				<h2 className='relative z-20 text-xl font-medium leading-5 text-t-gray'>PERSONAL PROJECT</h2>
-				<h1 className='font-bold relative z-20 xs:text-[2.5rem] text-4xl leading-10 mt-2'>Ready. Steady. Online.</h1>
-
+			<Link href='/projects/ready-steady-online'>
 				<motion.div
-					className='relative z-10 h-full'
-					variants={browser}
+					variants={card}
 					transition={transition}
+					initial='initial'
+					whileHover='hover'
+					className='relative flex flex-col w-full overflow-hidden bg-white pt-7 px-9 lg:grow lg:w-auto h-96 rounded-4xl hover:cursor-pointer'
+					onHoverStart={() => setHovering(true)}
+					onHoverEnd={() => setHovering(false)}
 				>
-					<Browser
-						className='lg:w-[150%] lg:scale-[80%] lg:-top-4 top-16 left-0 absolute'
-						url='readysteadbang.com'
-					>
-						<div className='relative w-full aspect-[1.6] bg-[#FAFAFA]'>
-							<Image
-								src='/images/ready-stand.png'
-								alt='Ready Steady Bang'
-								className='object-contain w-full h-full'
-								fill
-							/>
-						</div>
+					<h2 className='relative z-20 text-xl font-medium leading-5 text-t-gray'>PERSONAL PROJECT</h2>
+					<h1 className='font-bold relative z-20 xs:text-[2.5rem] text-4xl leading-10 mt-2'>Ready. Steady. Online.</h1>
 
-						<div className='absolute inset-0 z-20'>
-							<div className={clsx(
-								'relative w-full aspect-[1.6] bg-[#FAFAFA]',
-								hovering ? 'opacity-100' : 'opacity-0'
-							)}>
+					<motion.div
+						className='relative z-10 h-full'
+						variants={browser}
+						transition={transition}
+					>
+						<Browser
+							className='lg:w-[150%] lg:scale-[80%] lg:-top-4 top-16 left-0 absolute'
+							url='readysteadbang.com'
+						>
+							<div className='relative w-full aspect-[1.6] bg-[#FAFAFA]'>
 								<Image
-									src='/images/ready-fire.png'
+									src='/images/ready-stand.png'
 									alt='Ready Steady Bang'
 									className='object-contain w-full h-full'
 									fill
 								/>
 							</div>
-						</div>
 
-					</Browser>
+							<div className='absolute inset-0 z-20'>
+								<div className={clsx(
+									'relative w-full aspect-[1.6] bg-[#FAFAFA]',
+									hovering ? 'opacity-100' : 'opacity-0'
+								)}>
+									<Image
+										src='/images/ready-fire.png'
+										alt='Ready Steady Bang'
+										className='object-contain w-full h-full'
+										fill
+									/>
+								</div>
+							</div>
+
+						</Browser>
+					</motion.div>
 				</motion.div>
-			</motion.div>
+			</Link>
 
 			<Scribbles.CowboyHat tilted={hovering} />
 		</div>
--- src/components/projects/MD.tsx
+++ src/components/projects/MD.tsx
@@ -4,7 +4,7 @@ const MDComponents: Components = {
 	h1: ({ ...props }) => <h1 className='mb-6 text-3xl font-semibold' {...props} />,
 	h2: ({ ...props }) => <h2 className='mb-4 text-2xl font-semibold' {...props} />,
 	h3: ({ ...props }) => <h3 className='mb-2 text-xl font-semibold' {...props} />,
-	p: ({ ...props }) => <p className='mb-5 text-lg leading-7' {...props} />,
+	p: ({ ...props }) => <p className='mb-5 text-[1.0625rem] leading-[1.7rem]' {...props} />,
 	strong: ({ ...props }) => <strong className='font-semibold' {...props} />,
 	blockquote: ({ ...props }) => <blockquote className='text-lg font-normal italic text-gray-600 border-l-4 border-[#d2d2d2] pl-4 my-4' {...props} />,
 	a: ({ ...props }) => <a className='transition-opacity text-t-blue hover:opacity-80 hover:underline' {...props} />,
@@ -16,7 +16,9 @@ const MDComponents: Components = {
 			<img {...props} decoding="async" />
 			<div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-slate-900/10 dark:ring-white/10" />
 		</div>
-	)
+	),
+	ol: ({ ...props }) => <ol className='mb-4 list-decimal list-inside' {...props} />,
+	li: ({ ...props }) => <li className='mb-2 text-lg' {...props} />,
 }
 
 export default MDComponents;
