64dc87f5e7557474dbe0608dbc9d5eaa1bf13ec6
Josh Leichty <theodore.bersentes@gmail.com>
2023-07-02 12:43:37 -0400

    Making reverse proxy

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,12 @@ const nextConfig = {
 	images: {
 		domains: ["ucarecdn.com"],
 	},
+	rewrites: async () => [
+		{
+			source: "/ingest/:path*",
+			destination: "https://app.posthog.com/:path*",
+		},
+	],
 };
 
 export default withMDX(nextConfig);
