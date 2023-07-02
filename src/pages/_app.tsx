0faa9aed8168bd0f4f91b38a3e568a20d93514d8
Josh Leichty <theodore.bersentes@gmail.com>
2023-07-02 03:16:04 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ export const inter = Inter({
 if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
 	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
 		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+		autocapture: true,
 		loaded: (posthog) => {
 			if (process.env.NODE_ENV === 'development') posthog.debug()
 		}
