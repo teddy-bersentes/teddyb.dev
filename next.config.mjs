ce6b0b45de745daf9618a2113ea9f2ba21ea51f8
Theodore Bersentes <theodore.bersentes@gmail.com>
2024-08-09 09:13:11 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,26 @@ const withMDX = require("@next/mdx")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	skipTrailingSlashRedirect: true,
 	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 	images: {
 		domains: ["ucarecdn.com"],
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+			{
+				source: "/ingest/decide",
+				destination: "https://us.i.posthog.com/decide",
+			},
+		];
 	},
 };
 
